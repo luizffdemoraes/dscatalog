90951c450371ef9197583dcaa2ac49906990e55f
luizffdemoraes <lffm1994@gmail.com>
2025-06-10 23:55:41 -0300

    Exercício com repository

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTest.java
@@ -39,4 +39,26 @@ class ProductRepositoryTest {
         Optional<Product> result = repository.findById(existingId);
         Assertions.assertFalse(result.isPresent());
     }
+
+    @Test
+    void findShouldFindObjectWhenIdExists() {
+        existingId = 1L;
+        Product product = Factory.createProduct();
+        product.setId(existingId);
+
+        product = repository.save(product);
+
+        repository.findById(product.getId());
+        Optional<Product> result = repository.findById(product.getId());
+        Assertions.assertEquals(result.get(), product);
+    }
+
+    @Test
+    void findShouldReturnEmptyWhenIdDoesNotExist() {
+        Long nonExistingId = 999L;
+
+        Optional<Product> result = repository.findById(nonExistingId);
+
+        Assertions.assertTrue(result.isEmpty(), "Expected result to be empty when ID does not exist");
+    }
 }
